f8d4af6d48638d425cf27aab5fdcfa80c218f893
Leonardo <duckgames875@gmail.com>
2023-09-21 16:57:57 -0300

    Test

--- script.js
+++ script.js
@@ -3,39 +3,21 @@ $(document).ready(function () {
     let btn = $('.button-cta');
     let sizePass = $('#valor');
     let password = $('.passwordGenerated');
-    let containerPassword = $('.container-password')
-
-    let chatset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-
+    let containerPassword = $('.container-password');
+    let charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
 
     $(slider).on("input", function(){
-        $(sizePass).text(slider.val())
-    })
+        $(sizePass).text(slider.val());
+    });
 
     function generatePass(){
-        let pass = ""
-        for(let i = 0, n = chatset.length; i < $(slider).val(); i++){
-            pass += chatset.charAt(Math.floor(Math.random()*n));
+        let pass = "";
+        for(let i = 0, n = charset.length; i < $(slider).val(); i++){
+            pass += charset.charAt(Math.floor(Math.random()*n));
         }
         return pass;
     }
-    
-    $(btn).click(function (){
-        let pass = generatePass();
-        $(password).text(pass);
-        $(containerPassword).removeClass('hide');
-        $(password).val(pass);
-    })
 
-    $(password).click(function () {
-        const $tempInput = $("<input>");
-        $("body").append($tempInput);
-        $tempInput.val(pass).select();
-        document.execCommand("copy");
-        $tempInput.remove();
-    })
-
-    
     let clipboard = new ClipboardJS(btn[0], {
         text: function(trigger) {
             return generatePass();
@@ -50,5 +32,4 @@ $(document).ready(function () {
     clipboard.on('error', function(e) {
         console.error('Erro ao copiar a senha', e);
     });
-
-})
+});
