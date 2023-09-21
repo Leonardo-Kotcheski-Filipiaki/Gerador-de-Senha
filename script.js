7eb9d7b2c27fcaca2b2e3e2b83c1539a9aa3ccf9
Leonardo <duckgames875@gmail.com>
2023-09-21 17:10:03 -0300

    test

--- script.js
+++ script.js
@@ -3,33 +3,32 @@ $(document).ready(function () {
     let btn = $('.button-cta');
     let sizePass = $('#valor');
     let password = $('.passwordGenerated');
-    let containerPassword = $('.container-password');
-    let charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    let containerPassword = $('.container-password')
+
+    let chatset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+
 
     $(slider).on("input", function(){
-        $(sizePass).text(slider.val());
-    });
+        $(sizePass).text(slider.val())
+    })
 
     function generatePass(){
-        let pass = "";
-        for(let i = 0, n = charset.length; i < $(slider).val(); i++){
-            pass += charset.charAt(Math.floor(Math.random()*n));
+        let pass = ""
+        for(let i = 0, n = chatset.length; i < $(slider).val(); i++){
+            pass += chatset.charAt(Math.floor(Math.random()*n));
         }
         return pass;
     }
+    
+    $(btn).click(function (){
+        let pass = generatePass();
+        $(password).text(pass);
+        $(containerPassword).removeClass('hide');
+        $(password).val(pass);
+    })
 
-    let clipboard = new ClipboardJS(btn[0], {
-        text: function(trigger) {
-            return generatePass();
-        }
-    });
+    $(password).click(function () {
+        navigator.clipboard.writeText($(password).val());
+    })
 
-    clipboard.on('success', function(e) {
-        alert("Senha copiada para a área de transferência");
-        e.clearSelection();
-    });
-
-    clipboard.on('error', function(e) {
-        console.error('Erro ao copiar a senha', e);
-    });
-});
+})
